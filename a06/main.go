204fb4f00f12143858eba062a482de19b1d8a833
peitur <peitur@gmail.com>
2020-12-06 06:50:06 +0100

    wip

--- a06/main.go
+++ a06/main.go
@@ -7,11 +7,37 @@ import (
 //	"regexp"
 //	"math"
 //	"sort"
-//	"strings"
+	"strings"
 //	"strconv"
 )
 
+func parseGroupItems( group string ) map[byte]int {
+	var keys map[byte]int = map[byte]int{}
 
+	return keys
+}
+
+
+func parseGroups( data []string ) []string {
+
+	var groups []string = []string{}
+	pl := []string{}
+	for i := range( data ){
+		if len( data[i]) == 0 {
+			groups = append( groups, strings.Join( pl, "" ) )
+			pl = []string{}
+		}else{
+			pl = append( pl, strings.TrimSpace( data[i] ) )
+		}
+	}
+
+	if len( pl ) > 0{
+		groups = append( groups, strings.Join( pl, "" ) )
+	}
+	
+	return groups
+
+}
 
 func readFile( filename string ) ([]string, error) {
 	var ret []string = []string{}
@@ -44,6 +70,11 @@ func main( ) {
 	if err != nil {
 		fmt.Println("ERROR, no input file: ", err)
 		os.Exit(1)
+	}
+
+	groups := parseGroups( data )
+	for _, item := range( groups ){
+		fmt.Println( item )
 	}
 
 }
